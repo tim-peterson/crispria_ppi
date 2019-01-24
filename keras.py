2481c4b8329103846126b83e190f736c7be09e09
tim-peterson <petersontimr@gmail.com>
2019-01-24 15:51:43 -0600

    testing validity of PPI prediction model with MTOR vs. ATRAID interactors

--- keras.py
+++ keras.py
@@ -13,17 +13,17 @@ seed = 7
 numpy.random.seed(seed)
 # load pima indians dataset
 
-training_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-24-19-crispria_huttlin_trimmed_v4.csv", delimiter=",")
+training_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-24-19-crispria_huttlin_trimmed_v5.csv", delimiter=",")
 # skiprows=1,
 #, usecols = (1,15)
 #dataset = numpy.loadtxt(os.path.dirname(os.path.realpath(__file__)) + "/pima-indians-diabetes.data.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 
-X, Y = training_dataset[:, :-1], training_dataset[:, -1] # works the same as the commands below
+#X, Y = training_dataset[:, :-1], training_dataset[:, -1] # works the same as the commands below
 
-#X = training_dataset[:,0:14]
+X = training_dataset[:,0:14]
 
-#Y = training_dataset[:,14]
+Y = training_dataset[:,14]
 # create model
 model = Sequential()
 model.add(Dense(20, input_dim=14, activation='relu'))
@@ -49,7 +49,7 @@ model.fit(X, Y, epochs=5, batch_size=10,  verbose=2)
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-test_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-24-19-crispria_test_w_ATRAID.csv", delimiter=",")
+test_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-24-19-crispria_test_w_ATRAID_v4.csv", delimiter=",")
 #, usecols = (1,14)
 #test = training_dataset[:,0:13]
 # calculate predictions
@@ -58,10 +58,14 @@ predictions = model.predict(test_dataset)
 rounded = [round(x[0]) for x in predictions]
 print(rounded)
 
+#rounded.astype(numpy.int64)
+
+rounded = numpy.array(rounded, dtype=numpy.int64)
+
 test_dataset_v2 = test_dataset
 #[a[:,:j] for j in i]
-for i in range(test_dataset.shape[0]):
-	numpy.concatenate(test_dataset_v2[i], rounded[i])
+for i in range(int(test_dataset.shape[0])):
+	numpy.append(test_dataset_v2[i], rounded[i])
 #dictA = dict(zip(list1, rounded))
 
 #print(test_dataset_v2)
