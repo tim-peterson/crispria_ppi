ca61f5812e5842257e99cdc66fdfacfbf3d1ebea
tim-peterson <petersontimr@gmail.com>
2019-01-23 23:03:31 -0600

    getting training and test data prepared

--- keras.py
+++ keras.py
@@ -13,13 +13,13 @@ seed = 7
 numpy.random.seed(seed)
 # load pima indians dataset
 
-dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-23-19-crispra_huttlin_trimmed.csv", delimiter=",")
-
+training_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-23-19-crispra_huttlin_trimmed_v2.csv", delimiter=",")
+#test_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-23-19-crispra_test_trimmed.csv", delimiter=",")
 
 #dataset = numpy.loadtxt(os.path.dirname(os.path.realpath(__file__)) + "/pima-indians-diabetes.data.csv", delimiter=",")
 # split into input (X) and output (Y) variables
-X = dataset[:,0:14]
-Y = dataset[:,14]
+X = training_dataset[:,0:14]
+Y = training_dataset[:,14]
 # create model
 model = Sequential()
 model.add(Dense(20, input_dim=14, activation='relu'))
@@ -35,7 +35,7 @@ scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 # calculate predictions
-predictions = model.predict(X)
+predictions = model.predict(Y)
 # round predictions
 rounded = [round(x[0]) for x in predictions]
 print(rounded)
