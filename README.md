42efe6d58019b646b2439ab66715e03c429c3b42
tim-peterson <petersontimr@gmail.com>
2019-01-27 21:27:27 -0600

    add in dta screens sorted by stdev rho rather than pval, v11d

--- README.md
+++ README.md
@@ -22,8 +22,9 @@ got rid of sigmoid activation in output layer so predictions aren't binary but r
 
 v9 use Huttlin Compass scores rather than binary 0/1 binding no binding
 
-Result: debatable whether v8 or v9 is better. v8 (binary huttlin scoring) gets slightly more interactions
+Result: debatable whether v8 or v9 is better. v8 (binary huttlin scoring) gets slightly more interactions. Is raptor mTOR's best interactor?
 
-TO-DO: get probabilities for all 20,000 protein coding genes
+v10 TO-DO: We need negative controls to prove that random gene X like HSD17B12 doesn't just bind every gene. get probabilities for all 20,000 protein coding genes. 
 
+v11 test using std dev. outliers rather than p < 0.05 in training data counting towards huttlin positivity 
 compare vs. primary DNA/protein sequence-based predictions
--- keras0.py
+++ keras0.py
@@ -1,27 +1,19 @@
 # Create first network with Keras
 import os
-'''from random import random
-import pandas
-from pandas import concat
-from pandas import DataFrame'''
-
 import tensorflow
 from tensorflow import keras
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
-'''from tensorflow.python.keras.layers import LSTM
-from tensorflow.python.keras.layers import Masking'''
 
 #from keras.models import Sequential
 #from keras.layers import Dense
 import numpy
-from numpy import array
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 # load pima indians dataset
 
-training_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-27-19-crispria_huttlin_trimmed_v11d.csv", delimiter=",")
+training_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-27-19-crispria_huttlin_trimmed_v9.csv", delimiter=",")
 
 #training_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-25-19-crispria_huttlin_trimmed_v8.csv", delimiter=",")
 # skiprows=1,
@@ -30,17 +22,15 @@ training_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington Univ
 # split into input (X) and output (Y) variables
 
 #X, Y = training_dataset[:, :-1], training_dataset[:, -1] # works the same as the commands below
-X = training_dataset[:,0:16]
+X = training_dataset[:,0:14]
 #X = training_dataset[:,0:14]
-Y = training_dataset[:,16]
+Y = training_dataset[:,14]
 #Y = training_dataset[:,14]
 # create model
 model = Sequential()
-#model.add(Masking(mask_value=0, input_shape=(:,0:16)))
-#model.add(LSTM(5))
-model.add(Dense(20, input_dim=16, activation='relu'))
+model.add(Dense(20, input_dim=14, activation='relu'))
 #model.add(Dense(20, input_dim=14, activation='relu'))
-model.add(Dense(16, activation='relu'))
+model.add(Dense(14, activation='relu'))
 #model.add(Dense(14, activation='relu'))
 '''model.add(Dense(14, activation='relu'))
 model.add(Dense(14, activation='relu'))
@@ -63,7 +53,7 @@ model.fit(X, Y, epochs=5, batch_size=10,  verbose=2)
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-test_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-27-19-crispria_test_mtor_atraid_v11d.csv", delimiter=",")
+test_dataset = numpy.loadtxt("/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/MORPHEOME/PPI/morpheome_final-1-27-19-crispria_test_rap2a_metap2_v10.csv", delimiter=",")
 #, usecols = (1,14)
 #test = training_dataset[:,0:13]
 # calculate predictions
